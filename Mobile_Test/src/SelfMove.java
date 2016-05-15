b046f5ae9c8e18313c335fc266dc9a36e46f7155
Henry <tanksoga@gmail.com>
2016-05-15 17:11:23 +0800

    SelfMove

---
+++ Mobile_Test/src/SelfMove.java
@@ -0,0 +1,113 @@
+
+import java.io.IOException;
+
+import lejos.hardware.Button;
+import lejos.hardware.motor.EV3MediumRegulatedMotor;
+import lejos.hardware.port.MotorPort;
+import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3GyroSensor;
+import lejos.hardware.sensor.EV3UltrasonicSensor;
+import lejos.robotics.SampleProvider;
+
+public class SelfMove
+{
+     private float centerAngle = 0;
+     
+     private EV3MediumRegulatedMotor steerMotor = null;
+     private EV3MediumRegulatedMotor moveMotor = null;
+     private EV3UltrasonicSensor sonicSensor = null;
+     private EV3GyroSensor gyroSensor = null;
+     private SampleProvider angleProvider = null;
+
+     public SelfMove() throws IOException 
+     {
+          try
+          {
+               steerMotor = new EV3MediumRegulatedMotor(MotorPort.C);
+               moveMotor = new EV3MediumRegulatedMotor(MotorPort.B);
+               //sonicSensor = new EV3UltrasonicSensor(SensorPort.S2);
+               gyroSensor = new EV3GyroSensor(SensorPort.S3);
+               
+               angleProvider = gyroSensor.getAngleMode();
+               
+               System.out.println("Start...");
+               
+               //initSteerToCenter();
+               move();
+               gyroSensor.close();
+          }
+          catch(Exception e)
+          {
+               e.printStackTrace();
+               throw(e);
+          }
+     }
+     
+     private void move() throws IOException
+     {
+          try
+          {
+               moveMotor.setSpeed(4000);
+               moveMotor.backward();
+               
+               while (true)
+               {
+                    if (Button.ESCAPE.isDown())
+                    {
+                         moveMotor.stop();
+                    }
+                    
+                    float[] angle = { 0 };
+                    angleProvider.fetchSample(angle, 0);
+                    
+                    System.out.println("Angle" + angle[0]);
+                    
+                    gyroSensor.reset();
+                    
+                    Thread.sleep(100);
+               }
+          }
+          catch(Exception e)
+          {
+               e.printStackTrace();
+          }
+     }
+     
+     private void initSteerToCenter() throws IOException 
+     {
+          try
+          {
+               steerMotor.forward();               
+               while(steerMotor.isMoving()){}
+               steerMotor.stop(true);            
+               float maxRightAngle = steerMotor.getTachoCount();               
+               steerMotor.resetTachoCount();
+               
+               steerMotor.backward();               
+               while(steerMotor.isMoving()){}
+               steerMotor.stop(true);
+               float maxLeftAngle = steerMotor.getTachoCount();               
+               steerMotor.resetTachoCount();
+               
+               centerAngle = maxLeftAngle / 2;
+               steerMotor.rotate(Math.abs(Math.round(centerAngle)));
+               while(steerMotor.isMoving()){}
+               steerMotor.stop(true);
+               
+               System.out.println("Max right angle=" + maxRightAngle);
+               System.out.println("Max left angle=" + maxLeftAngle);
+               System.out.println("Center angle=" + Math.abs(Math.round(centerAngle)));
+          }
+          catch(Exception e)
+          {
+               e.printStackTrace();
+               throw(e);
+          }
+     }
+
+     public static void main(String[] args) throws IOException 
+     {
+          new SelfMove();
+     }
+
+}
